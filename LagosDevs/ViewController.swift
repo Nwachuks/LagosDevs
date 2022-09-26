49dc1833b292ccd47a197e7a22abd72fe91721bf
Nwachukwu <nwachukwu@cowrywise.com>
2022-09-26 14:40:13 +0100

    Xcode setup

---
+++ LagosDevs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LagosDevs
+//
+//  Created by Nwachukwu Ejiofor on 26/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
